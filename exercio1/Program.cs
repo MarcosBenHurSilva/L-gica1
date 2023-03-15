9b3bb1861e506e45ef56f284ced56dc38b699f28
MarcosBenHurSilva <127806006+MarcosBenHurSilva@users.noreply.github.com>
2023-03-15 19:08:25 -0300

    Add files via upload

---
+++ exercio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+internal class Program {
+
+    private static void Main(string[] args) {
+
+        Console.Write("Qual o códido da primeira peça: ");
+        int cod_1 = int.Parse(Console.ReadLine());       
+        Console.Write("Digite o número de peças: ");
+        float quant_1 = float.Parse(Console.ReadLine());
+        Console.Write("Digite o número de peças: ");
+        float val_1 = float.Parse(Console.ReadLine());
+        Console.Write("Qual o códido da segunda peça: ");
+        int cod_2 = int.Parse(Console.ReadLine());
+        Console.Write("Digite o número de peças: ");
+        float quant_2 = float.Parse(Console.ReadLine());
+        Console.Write("Digite o número de peças: ");
+        float val_2 = float.Parse(Console.ReadLine());
+
+        float soma_1 = val_1 * quant_1;
+        float soma_2 = val_2 * quant_2;
+        float SomaPecas = soma_1 + soma_2;
+
+
+        Console.Write("VALOR A PAGAR: R$ " + SomaPecas);
+    }
+}
