55620a88b81794f1cf1a05a88645a3f6b60f8756
MarcosBenHurSilva <127806006+MarcosBenHurSilva@users.noreply.github.com>
2023-03-15 20:40:53 -0300

    Add files via upload

---
+++ Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+
+internal class Program {
+    private static void Main(string[] args) {
+        Console.Write("Escreva a variável a: ");
+        double a = float.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("Escreva a variável b: ");
+        double b = float.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("Escreva a variável c: ");
+        double c = float.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        double delta = (b * b) - (4 * a * c);
+        double X1 = ((-1 * b) + Math.Sqrt(delta)) / (2 * a);
+        double X2 = ((-1 * b) - Math.Sqrt(delta)) / (2 * a);
+
+        Console.WriteLine("X1 = " + X1);
+        Console.WriteLine("X2 = " + X2);
+    }
+}
