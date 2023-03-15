62307ec2d9c4e747ac6d5ea5de6e75a9be85ef0a
MarcosBenHurSilva <127806006+MarcosBenHurSilva@users.noreply.github.com>
2023-03-15 19:30:29 -0300

    Add files via upload

---
+++ exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+internal class Program {
+    private static void Main(string[] args) {
+        while (true) {
+            Console.Write("Qual o raio do circulo: ");
+            float raio = float.Parse(Console.ReadLine());
+
+            float raio2 = raio * raio;
+            float A = (float)Math.PI * raio2;
+
+
+            Console.Write("A = " + A);
+            Console.WriteLine();
+        }
+    }
+}
