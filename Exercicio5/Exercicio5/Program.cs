abe14c3ae367ed193730fde2fdf42440aa3ba7c0
MarcosBenHurSilva <127806006+MarcosBenHurSilva@users.noreply.github.com>
2023-03-15 21:24:33 -0300

    Add files via upload

---
+++ Exercicio5/Exercicio5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+internal class Program {
+    private static void Main(string[] args) {
+
+        int senha_correta = 2002;
+        int senha;
+
+        do {
+            Console.Write("Digite a senha: ");
+            senha = int.Parse(Console.ReadLine());
+
+            if (senha != senha_correta) {
+                Console.WriteLine("Senha Inválida");
+            }
+
+        } while (senha != senha_correta);
+
+        Console.WriteLine("Acesso Permitido");
+    }
+}
